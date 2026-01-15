2c30aa23cc8a10a5ce0af776a9087d120a1e35c3
Khoudia Niang <niang62@yahoo.com>
2026-01-15 19:11:45 +0000

    Creation des migrations et models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['patient', 'doctor', 'receptionist'])->default('patient');
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2026_01_15_184528_create_prescriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('appointment_id')->constrained('appointments')->onDelete('cascade');
+            $table->text('diagnosis');
+            $table->text('medications'); // Liste des médicaments prescrits
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
