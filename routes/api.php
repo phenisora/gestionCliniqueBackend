b6fd8d1ce4f973044f4ac2e7a8565a8e0bbc18f4
saydiatou <diatoufall394@gmail.com>
2026-01-19 19:49:10 +0000

    CRUD PATIENT

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//Route::group(['middleware'=>'auth:sanctum'], function(){
+    Route::GET('/patients', [PatientController::class, 'index']);
+    Route::GET('/patients/{id}', [PatientController::class, 'show']);
+    Route::DELETE('/patients/{id}', [PatientController::class, 'destroy']);
+    Route::PUT('/patients/{id}', [PatientController::class, 'update']);
+    Route::GET('/patients/{id}/medical-history', [PatientController::class, 'medicalHistory']);
+    Route::GET('/patients/{id}/appointments', [PatientController::class, 'appointments']);
+//});
