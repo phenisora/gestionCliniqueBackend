e68a29d833c31db3f9bd7adb99fbe5dd20407a01
Moi Khady <khadykebecamara@example.com>
2026-01-25 23:41:16 +0100

    Creation CRUD ordonnance partie 1

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prescription;
 use Illuminate\Http\Request;
 
 class PrescriptionController extends Controller
 {
-    
+    public function creerOrdonnance(Request $request){
+        $data = $request->validate([
+
+          'appointment_id' => 'required',
+            'diagnosis'      => 'required',
+            'medications'    => 'required',
+            'notes'          => 'nullable'
+        ]);
+
+        $prescription = Prescription::create($data);
+
+        return response()->json([
+            'message' => 'Ordonnance créée avec succès',
+            'data' => $prescription
+        ], 201);
+    }
+
+    public function modifierOrdonnance(Request $request, $id){
+        $prescription = Prescription::findOrFail($id);
+        
+        // On met à jour 
+        $prescription->update($request->all());
+
+        return response()->json([
+            'message' => 'Ordonnance modifiée avec succés',
+            'data' => $prescription
+        ]);
+
+    }
+
+    public function supprimerOrdonnance(){
+        $prescription = Prescription::findOrFail($id);
+        $prescription->delete();
+
+        return response()->json([
+            'message' => 'Ordonnance supprimée avec succès'
+        ]);
+
+    }
 }
