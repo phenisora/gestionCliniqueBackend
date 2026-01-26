fd8ceb7d30c06b8b7e41ecc8d5be3962518d239d
Khoudia Niang <niang62@yahoo.com>
2026-01-26 21:35:55 +0000

    gestion de changes

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
-
-use App\Http\Controllers\AuthController;
-
-
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\PrescriptionController;
@@ -16,13 +9,6 @@ use App\Http\Controllers\PrescriptionController;
 
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -39,19 +25,7 @@ Route::get('/doctors/available', [DoctorController::class, 'availableParDate']);
 
 // Routes Protégées (Médecin/Réceptionniste)
 
-<<<<<<< HEAD
 Route::middleware('auth:sanctum')->group(function () {
-=======
-
-Route::middleware('auth:api')->group(function () {
-
-
-//Route::middleware('auth:sanctum')->group(function () {
-
-
-//Route::middleware('auth:sanctum')->group(function () {
-
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
     Route::post('/doctors', [DoctorController::class, 'store']);
     Route::put('/doctors/{doctors}', [DoctorController::class, 'update']);
     Route::delete('/doctors/{id}', [DoctorController::class, 'supprimer']);
@@ -60,37 +34,22 @@ Route::middleware('auth:api')->group(function () {
 });
 
 
-<<<<<<< HEAD
-Route::group(['middleware'=>'auth:sanctum'], function(){
-
-=======
 
 Route::group(['middleware'=>'auth:api'], function(){
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
     Route::GET('/patients', [PatientController::class, 'index']);
     Route::GET('/patients/{id}', [PatientController::class, 'show']);
     Route::DELETE('/patients/{id}', [PatientController::class, 'destroy']);
     Route::PUT('/patients/{id}', [PatientController::class, 'update']);
     Route::GET('/patients/{id}/medical-history', [PatientController::class, 'medicalHistory']);
     Route::GET('/patients/{id}/appointments', [PatientController::class, 'appointments']);
-<<<<<<< HEAD
-
 });
-
-=======
-});
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
 
 Route::post('/auth/register', [AuthController::class, 'registerPatient']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 
-<<<<<<< HEAD
-Route::middleware(['auth:sanctum'])->group(function () {
-=======
 Route::middleware(['auth:api'])->group(function () {
 
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('auth/profile', [AuthController::class, 'profile']);
     Route::put('auth/updateProfile', [AuthController::class, 'updateProfile']);
@@ -102,8 +61,6 @@ Route::middleware(['auth:api'])->group(function () {
     });
 });
 
-<<<<<<< HEAD
-=======
 
 
 //Route pour les ordonnances
@@ -111,4 +68,3 @@ Route::middleware(['auth:api'])->group(function () {
 Route::post('/prescriptions',[PrescriptionController::class,'creerOrdonnance']);
 Route::put('/prescriptions/{id}',[PrescriptionController::class,'modifierOrdonnance']);
 Route::delete('/prescriptions/{id}',[PrescriptionController::class,'supprimerOrdonnance']);
->>>>>>> 2c9541720b0fe031ea93e59a9a71fd21463abb34
